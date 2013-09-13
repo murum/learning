e1835d9acd225cb099cc20ef00567a963b1534ba
murum <cr222cs@student.lnu.se>
2013-09-13 17:33:04 +0200

    added oswald font as default

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Christoffer Rydberg utforskar nya arenor</title>
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="/assets/css/main.css">
 </head>	
 <body>
 	<h1>Rolig sida?!?!</h1>
