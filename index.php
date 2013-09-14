c97fe24c65e1108e3496f5ab533d225e4e6b5769
murum <cr222cs@student.lnu.se>
2013-09-14 16:19:21 +0200

    added css styles with comments for structure

--- index.php
+++ index.php
@@ -6,11 +6,22 @@
 	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="/assets/css/bootstrap-theme.css">
 	<link rel="stylesheet" href="/assets/css/main.css">
 </head>	
 <body>
-	<h1>Git-Deploy snurrar om jag ser detta!</h1>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<h1>Git-Deploy snurrar om jag ser detta!</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<a href="/" class="btn btn-success">hem</a>
+			</div>
+		</div>
+	</div>
 	<!-- Latest compiled bootstrap and minified JavaScript -->
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </body>
